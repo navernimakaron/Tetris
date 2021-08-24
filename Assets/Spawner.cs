cfe00254c14db8df346bc832d1dc767a5987ddfe
Anton Potapov <anton.potapov@tinybuild.com>
2021-08-24 13:56:55 +0400

    Coroutine apples test
    Tried to make coroutines and delegates work together

---
+++ Assets/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameObject;
+    [SerializeField] private float _spawnGap = 3f;
+
+    private delegate void upForce();
+
+    private upForce force;
+
+    private IEnumerator _coroutine()
+    {
+        yield return new WaitForSeconds(_spawnGap);
+        Instantiate(_gameObject);
+        force();
+    }
+
+    private void Start()
+    {
+        var rigidBody = _gameObject.GetComponent<Rigidbody>();
+        force = Side;
+    }
+
+    private void Update()
+    {
+        StartCoroutine("_coroutine");
+        StopCoroutine("_coroutine");
+    }
+
+    private void Side()
+    {
+        var rigidBody = _gameObject.GetComponent<Rigidbody>();
+        rigidBody.AddForce(Vector3.left);
+        Debug.Log("Side!");
+        force = Up;
+    }
+
+    private void Up()
+    {
+        var rigidBody = _gameObject.GetComponent<Rigidbody>();
+        rigidBody.AddForce(Vector3.up);
+        Debug.Log("Up!");
+        force = Side;
+    }
+}
